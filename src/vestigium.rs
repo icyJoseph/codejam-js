a962146c37c56d77cde627a85075d896321411ac
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-01-11 14:47:17 +0100

    refactor: run rustfmt on vestigium

--- src/vestigium.rs
+++ src/vestigium.rs
@@ -1,18 +1,18 @@
 // https://codingcompetitions.withgoogle.com/codejam/round/000000000019fd27/000000000020993c
 use std::io;
 
-type Res<T> =  Result<T, Box<dyn std::error::Error>>;
+type Res<T> = Result<T, Box<dyn std::error::Error>>;
 
 fn nxt() -> String {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_) => input,
-        _ => panic!("Error reading line")
+        _ => panic!("Error reading line"),
     }
 }
 
 fn ptc<T: std::str::FromStr>() -> T {
-    match nxt().trim().parse::<T>(){
+    match nxt().trim().parse::<T>() {
         Ok(n) => n,
         _ => panic!("Error parsing"),
     }
@@ -23,37 +23,44 @@ fn main() -> Res<()> {
 
     for case in 1..=n {
         let rows = ptc::<usize>();
-       
+
         let mut trace = 0;
-        
-        let mut grid:Vec<Vec<i32>> = vec![vec![0;rows];rows];
+
+        let mut grid: Vec<Vec<i32>> = vec![vec![0; rows]; rows];
 
         use std::collections::HashSet;
-        
-        let mut rep_rows:HashSet<usize> = HashSet::new();
-        let mut rep_cols:HashSet<usize> = HashSet::new();
+
+        let mut rep_rows: HashSet<usize> = HashSet::new();
+        let mut rep_cols: HashSet<usize> = HashSet::new();
 
         for row in 0..rows {
-            for (i, cell) in nxt().split_whitespace().enumerate(){
+            for (i, cell) in nxt().split_whitespace().enumerate() {
                 let val = cell.parse::<i32>()?;
                 if grid[row].contains(&val) {
-                   rep_rows.insert(row);
+                    rep_rows.insert(row);
                 };
-                
-                let col =  grid.iter().map(|r| r[i]).collect::<Vec<i32>>();
-                
+
+                let col = grid.iter().map(|r| r[i]).collect::<Vec<i32>>();
+
                 if col.contains(&val) {
                     rep_cols.insert(i);
                 };
-                
+
                 grid[row][i] = val;
 
-                if row == i {trace = trace + val};
+                if row == i {
+                    trace = trace + val
+                };
             }
-
         }
-        
-        println!("Case #{}: {} {} {}", case, trace, rep_rows.len(), rep_cols.len());
+
+        println!(
+            "Case #{}: {} {} {}",
+            case,
+            trace,
+            rep_rows.len(),
+            rep_cols.len()
+        );
     }
 
     Ok(())
