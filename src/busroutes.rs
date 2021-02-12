6f7fb8c6b0e7b6a2f45c77689aa114a316587400
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-02-12 08:06:03 +0100

    feat: kick start bus routes

---
+++ src/busroutes.rs
@@ -0,0 +1,47 @@
+use std::io;
+
+type Res<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn nxt() -> String {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => input,
+        _ => panic!("Error reading line"),
+    }
+}
+
+fn parse_num<T: std::str::FromStr>() -> T {
+    match nxt().trim().parse::<T>() {
+        Ok(n) => n,
+        _ => panic!("Error parsing"),
+    }
+}
+
+fn parse_vec<T: std::str::FromStr>() -> Vec<T> {
+    nxt()
+        .split_whitespace()
+        .map(|x| match x.parse::<T>() {
+            Ok(n) => n,
+            _ => panic!("Could not parse vector"),
+        })
+        .collect()
+}
+
+fn main() -> Res<()> {
+    let n = parse_num::<u32>();
+    for case in 1..=n {
+        let spec = parse_vec::<u64>();
+        let buses = parse_vec::<u64>();
+
+        let mut d = spec[1];
+
+        for b in (0..buses.len()).rev() {
+            let bus = buses[b];
+            let step_back = d % bus;
+            d -= step_back;
+        }
+
+        println!("Case #{}: {}", case, d);
+    }
+    Ok(())
+}
