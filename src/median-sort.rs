9fa96d8b70fcfd07b54c374f7da981b6b3a91833
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-03-27 18:23:45 +0100

    feat: Median sort test case 2

--- src/median-sort.rs
+++ src/median-sort.rs
@@ -38,6 +38,51 @@ fn print_triplet(triplet: &(usize, usize, usize)) -> String {
     format!("{} {} {}", triplet.0 + 1, triplet.1 + 1, triplet.2 + 1)
 }
 
+fn binary_search(body: &Vec<usize>, target: usize) -> usize {
+    let mut lower = 0;
+    let mut upper = body.len() / 2;
+
+    if body.len() == 0 {
+        return 1;
+    }
+
+    if body.len() == 1 {
+        return 1;
+    }
+
+    if body.len() == 2 {
+        println!("{}", print_triplet(&(body[lower], body[upper], target)));
+
+        let answer = parse_num::<usize>() - 1;
+
+        if answer == target {
+            return lower + 1;
+        } else if answer == body[lower] {
+            return lower;
+        } else if answer == body[upper] {
+            return upper + 1;
+        }
+    }
+
+    println!("{}", print_triplet(&(body[lower], body[upper], target)));
+
+    let answer = parse_num::<usize>() - 1;
+
+    if answer == target {
+        // target is between lower and upper
+        let index = binary_search(&body[lower..upper].to_vec(), target);
+        return lower + index;
+    } else if answer == body[lower] {
+        return 0;
+    } else if answer == body[upper] {
+        // c must be between upper and the end of body
+        let index = binary_search(&body[upper..].to_vec(), target);
+        return upper + index;
+    } else {
+        panic!("Unexpected result");
+    }
+}
+
 fn main() -> Res<()> {
     let spec = parse_vec::<usize>();
 
@@ -58,26 +103,8 @@ fn main() -> Res<()> {
             let answer = parse_num::<usize>() - 1;
 
             if answer == body[0] {
-                let mut pointer = 1;
-
-                loop {
-                    if pointer == body.len() {
-                        body.push(next);
-                        break;
-                    }
-                    println!("{}", print_triplet(&(body[0], body[pointer], next)));
-
-                    let median = parse_num::<usize>() - 1;
-
-                    if median == body[pointer] {
-                        // do nothing
-                    } else if median == next {
-                        body.insert(pointer, next);
-                        break;
-                    }
-
-                    pointer += 1;
-                }
+                let index = binary_search(&body, next);
+                body.insert(index, next);
             } else if answer == head {
                 body.insert(0, head);
                 head = next;
