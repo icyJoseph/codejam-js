30b3279f04f4adc12592cfb5155da3e4cc008575
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-03-27 13:42:17 +0100

    feat: Reversort Engineering 2021 part 2

--- src/reversort-engineering.rs
+++ src/reversort-engineering.rs
@@ -92,6 +92,78 @@ fn search(
     return None;
 }
 
+fn reverse_section(list: &Vec<usize>, from: usize, to: usize) -> Vec<usize> {
+    let mut answer: Vec<usize> = vec![];
+
+    for index in 0..from {
+        answer.push(list[index]);
+    }
+
+    for index in (from..=to).rev() {
+        answer.push(list[index]);
+    }
+
+    for index in to + 1..list.len() {
+        answer.push(list[index]);
+    }
+
+    return answer;
+}
+
+fn rev_cost(n: usize, target: usize) -> Option<Vec<usize>> {
+    if target < n - 1 {
+        return None;
+    }
+    if target > n * (n + 1) / 2 - 1 {
+        return None;
+    }
+
+    // special cases
+    if target == n - 1 {
+        let answer = (1..=n).collect();
+        return Some(answer);
+    }
+
+    if target == n {
+        let mut answer: Vec<usize> = (1..=n).collect();
+
+        let last_index = answer.len() - 1;
+        answer.swap(last_index, last_index - 1);
+
+        return Some(answer);
+    }
+
+    // build the vector
+    let mut comps: Vec<usize> = (2..=n).collect();
+    let mut curr = 0;
+
+    loop {
+        let sum = comps.iter().sum::<usize>();
+
+        if sum == target {
+            break;
+        }
+
+        if comps[curr] > 1 {
+            comps[curr] = comps[curr] - 1;
+            continue;
+        }
+        curr += 1;
+    }
+
+    let mut answer: Vec<usize> = (1..=n).collect();
+    let mut pointer = n - 2;
+
+    for inst in comps.iter() {
+        answer = reverse_section(&answer, pointer, pointer + inst - 1);
+        if pointer > 0 {
+            pointer -= 1;
+        }
+    }
+
+    Some(answer)
+}
+
 fn main() -> Res<()> {
     let n = parse_num::<u32>();
     for case in 1..=n {
@@ -100,17 +172,26 @@ fn main() -> Res<()> {
         let upper = spec[0];
         let cost = spec[1];
 
-        let list: Vec<usize> = (1..=upper).collect();
+        if upper <= 7 {
+            let mut permutation: Vec<usize> = vec![];
+            let mut chosen = vec![false; upper];
 
-        let mut permutation: Vec<usize> = vec![];
-        let mut chosen = vec![false; upper];
-
-        match search(&mut permutation, &mut chosen, upper, cost) {
-            Some(l) => {
-                println!("Case #{}: {}", case, print_ls(l));
+            match search(&mut permutation, &mut chosen, upper, cost) {
+                Some(l) => {
+                    println!("Case #{}: {}", case, print_ls(l));
+                }
+                None => {
+                    println!("Case #{}: {}", case, "IMPOSSIBLE");
+                }
             }
-            None => {
-                println!("Case #{}: {}", case, "IMPOSSIBLE");
+        } else {
+            match rev_cost(upper, cost) {
+                Some(l) => {
+                    println!("Case #{}: {}", case, print_ls(l));
+                }
+                None => {
+                    println!("Case #{}: {}", case, "IMPOSSIBLE");
+                }
             }
         }
     }
