5127f2b301b645c3640fcb2c3a1ae4a2a33756e3
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-08-22 14:53:40 +0200

    feat: Kickstart 2022 round E - coloring game

---
+++ src/coloring_game.rs
@@ -0,0 +1,50 @@
+use std::io;
+
+fn main() -> Res<()> {
+    let n = parse_num::<u32>();
+    for case in 1..=n {
+        let total_cells = parse_num::<u64>();
+
+        let bot_score = (total_cells + 4) / 5;
+
+        println!("Case #{}: {}", case, bot_score);
+    }
+
+    Ok(())
+}
+
+type Res<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn nxt() -> String {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => input,
+        _ => panic!("Error reading line"),
+    }
+}
+
+fn parse_num<T: std::str::FromStr>() -> T {
+    match nxt().trim().parse::<T>() {
+        Ok(n) => n,
+        _ => panic!("Error parsing"),
+    }
+}
+
+#[allow(dead_code)]
+fn parse_vec<T: std::str::FromStr>() -> Vec<T> {
+    nxt()
+        .split_whitespace()
+        .map(|x| match x.parse::<T>() {
+            Ok(n) => n,
+            _ => panic!("Could not parse vector"),
+        })
+        .collect()
+}
+
+#[allow(dead_code)]
+fn string_vec<T: std::string::ToString>(vec: &Vec<T>, separator: &str) -> String {
+    vec.iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(separator)
+}
