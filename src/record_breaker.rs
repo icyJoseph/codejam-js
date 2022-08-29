2b938c72c58951297463785c95a3914afc59d658
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-08-29 12:35:50 +0200

    feat: Kickstart practice session 3 - improve record breaker solution

--- src/record_breaker.rs
+++ src/record_breaker.rs
@@ -1,5 +1,45 @@
 use std::io;
 
+fn main() -> Res<()> {
+    let n = parse_num::<u32>();
+    for case in 1..=n {
+        let _ = parse_num::<usize>();
+
+        let days = parse_vec::<usize>();
+
+        let mut peak: Option<usize> = None;
+
+        let mut record_days = 0;
+
+        for i in 0..days.len() {
+            let first_condition = i == 0 || {
+                match peak {
+                    Some(m) if days[i] <= m => false,
+                    _ => true,
+                }
+            };
+
+            let second_condition = i == days.len() - 1 || { days[i] > days[i + 1] };
+
+            if first_condition && second_condition {
+                record_days += 1;
+            }
+
+            match peak {
+                None => peak = Some(days[i]),
+                Some(m) if m < days[i] => {
+                    peak = Some(days[i]);
+                }
+                _ => {}
+            }
+        }
+
+        println!("Case #{}: {}", case, record_days);
+    }
+
+    Ok(())
+}
+
 type Res<T> = Result<T, Box<dyn std::error::Error>>;
 
 fn nxt() -> String {
@@ -17,6 +57,7 @@ fn parse_num<T: std::str::FromStr>() -> T {
     }
 }
 
+#[allow(dead_code)]
 fn parse_vec<T: std::str::FromStr>() -> Vec<T> {
     nxt()
         .split_whitespace()
@@ -27,38 +68,10 @@ fn parse_vec<T: std::str::FromStr>() -> Vec<T> {
         .collect()
 }
 
-fn main() -> Res<()> {
-    let n = parse_num::<u32>();
-    for case in 1..=n {
-        let total = parse_num::<usize>();
-        let days = parse_vec::<u64>();
-
-        let mut max = days[0];
-        let mut count = 0;
-
-        if total > 1 {
-            if max > days[1] {
-                count += 1;
-            }
-        } else {
-            count += 1;
-        }
-
-        for day in 1..total {
-            let current = days[day];
-
-            if current > max {
-                max = current;
-
-                if day == total - 1 {
-                    count += 1;
-                } else if current > days[day + 1] {
-                    count += 1;
-                }
-            }
-        }
-
-        println!("Case #{}: {}", case, count);
-    }
-    Ok(())
+#[allow(dead_code)]
+fn string_vec<T: std::string::ToString>(vec: &Vec<T>, separator: &str) -> String {
+    vec.iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(separator)
 }
